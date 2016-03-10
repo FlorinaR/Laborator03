383d33e6a2b0924f900caf227988782265b7e6bc
Florina <florina.roaua@gmail.com>
2016-03-10 16:36:28 +0200

    tasks done for Laboratory 03

--- PhoneDialer/src/ro/pub/cs/systems/pdsd/lab03/phonedialer/PhoneDialerActivity.java
+++ PhoneDialer/src/ro/pub/cs/systems/pdsd/lab03/phonedialer/PhoneDialerActivity.java
@@ -1,12 +1,16 @@
 package ro.pub.cs.systems.pdsd.lab03.phonedialer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 
 public class PhoneDialerActivity extends Activity {
@@ -16,10 +20,13 @@ public class PhoneDialerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_dialer);
+        //change the orientation 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         
     	int[] buttonIds = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn4,
     		R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10, R.id.btn11};
     	final EditText phoneNumber = (EditText)findViewById(R.id.editText1);
+ 
     	for(int i = 0; i <  buttonIds.length; i++) {
     		final Button btn = (Button)findViewById(buttonIds[i]);
     		
@@ -28,8 +35,57 @@ public class PhoneDialerActivity extends Activity {
 				@Override
 				public void onClick(View v) {
 					// TODO Auto-generated method stub
-					String message = btn.getText().toString();
-					phoneNumber.setText(phoneNumber.getText().toString() + message);
+					String number = btn.getText().toString();
+					phoneNumber.setText(phoneNumber.getText().toString() + number);
+					/* Place the cursor in the end of EditText */
+			    	phoneNumber.setSelection(phoneNumber.length(), phoneNumber.length());
+				}
+			});
+    	}
+    	/* Backspace functionality */
+    	final ImageButton backspace_btn = (ImageButton)findViewById(R.id.backspace);
+    	if(phoneNumber.getText().toString() != " ") {
+    		backspace_btn.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					String number = phoneNumber.getText().toString();
+					if(number.length() > 0) {
+						number = number.substring(0, number.length() - 1);
+						phoneNumber.setText(number);
+						/* Place the cursor in the end of EditText */
+				    	phoneNumber.setSelection(phoneNumber.length(), phoneNumber.length());
+					}
+				}
+			});
+    		
+    	}
+    	
+    	/* Call functionality */
+    	ImageButton call_btn = (ImageButton) findViewById(R.id.call);
+    	if (phoneNumber.getText().toString() != " ") {
+    		call_btn.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Intent intent = new Intent(Intent.ACTION_CALL);
+					intent.setData(Uri.parse("tel:"+ phoneNumber));
+					startActivity(intent);
+				}
+			});
+    	}
+    	
+    	/* Hang-up functionality */
+    	ImageButton hangup_btn = (ImageButton) findViewById(R.id.hangup);
+    	if (phoneNumber.getText().toString() != " ") {
+    		hangup_btn.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					finish();
 				}
 			});
     	}
